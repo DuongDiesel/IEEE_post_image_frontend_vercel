67eb6c65319d222ebf55e434f5d67e1f1c5f47d6
DuongDiesel <duongbgvip2.com>
2022-10-16 12:08:14 +0900

    add image1

--- src/components/Classifier/Classifier.js
+++ src/components/Classifier/Classifier.js
@@ -4,6 +4,8 @@ import './Classifier.css'
 import {Spinner, Button, Alert, Image} from 'react-bootstrap'
 import axios from 'axios'
 
+import pic1 from ".logo192.png"
+
 class Classifier extends Component {
     state = {
         files: [],
@@ -101,37 +103,42 @@ class Classifier extends Component {
             </li>
           ));
         return (
+            <>
             <Dropzone onDrop={this.onDrop} accept='image/png, image/jpeg'>
-            {({isDragActive, getRootProps, getInputProps}) => (
-              <section className="container">
-                <div {...getRootProps({className: 'dropzone back'})}>
-                  <input {...getInputProps()} />
-                  <i className="far fa-image mb-2 text-muted" style={{fontSize:100}}></i>
-                  <p className='text-muted'>{isDragActive ? "Drop some images " : "Drag 'n' drop some files here, or click to select files"}</p>
-                </div>
-                <aside>
-                  {files}
-                </aside>
+                {({ isDragActive, getRootProps, getInputProps }) => (
+                    <section className="container">
+                        <div {...getRootProps({ className: 'dropzone back' })}>
+                            <input {...getInputProps()} />
+                            <i className="far fa-image mb-2 text-muted" style={{ fontSize: 100 }}></i>
+                            <p className='text-muted'>{isDragActive ? "Drop some images " : "Drag 'n' drop some files here, or click to select files"}</p>
+                        </div>
+                        <aside>
+                            {files}
+                        </aside>
 
-                {this.state.files.length >0 &&
-                <Button variant='info' size='lg' className='mt-3' onClick={this.sendImage}>Select Image</Button>
-                }
+                        {this.state.files.length > 0 &&
+                            <Button variant='info' size='lg' className='mt-3' onClick={this.sendImage}>Select Image</Button>}
 
-                {this.state.isLoading &&
-                <Spinner animation="border" role="status"></Spinner>
-                }
+                        {this.state.isLoading &&
+                            <Spinner animation="border" role="status"></Spinner>}
 
-                {this.state.recentImage &&
-                <React.Fragment>
-                    <Alert variant='primary'>
-                        {this.state.recentImage.data.classified}
-                    </Alert>
-                    <Image className='justify-content-center' src={this.state.recentImage.data.picture} height='200' rounded/>
-                </React.Fragment>
-                }
-              </section>
-            )}
-          </Dropzone>
+                        {this.state.recentImage &&
+                            <React.Fragment>
+                                <Alert variant='primary'>
+                                    {this.state.recentImage.data.classified}
+                                </Alert>
+                                <Image className='justify-content-center' src={this.state.recentImage.data.picture} height='200' rounded />
+                            </React.Fragment>}
+                    </section>
+                )}
+            </Dropzone>
+
+            <React.Fragment>
+                <img src="https://github.com/duongdiesel/imageUpload/blob/main/00000001_001.png" />
+            </React.Fragment>
+            
+            </>
+          
         );
     }
 }
